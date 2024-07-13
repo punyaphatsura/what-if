25f697bd2b5f6b4bd5f7c5e435eeb64bb023feac
KorawutMing <6432004421@student.chula.ac.th>
2024-07-13 12:46:42 +0700

    feat: :sparkles: basic page transition logic

--- src/components/MainFlow.tsx
+++ src/components/MainFlow.tsx
@@ -1,7 +1,29 @@
+'use client'
+
 import React from "react";
 
 const MainFlow = () => {
-  return <div className="bg-green-500">MainFlow</div>;
+  const [pageIdx, setPageIdx] = React.useState('P1');
+  if (['P1', 'P2', 'P3', 'P8'].includes(pageIdx)) {
+    return <TransitionPage pageIdx={pageIdx} setPageIdx={setPageIdx} />;
+  } else {
+    return <QuestionPage pageIdx={pageIdx} setPageIdx={setPageIdx} />;
+  }
 };
+
+const TransitionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: (idx: string) => void }) => {
+  return (
+    <div className="bg-green-500">
+      <h1>{pageIdx}</h1>
+      <button onClick={() => setPageIdx('P2')}>Go to next page</button>
+    </div>
+  );
+};
+
+const QuestionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: (idx: string) => void }) => {
+  return (
+    <div className="bg-blue-500">QuestionPage</div>
+  );
+}
 
 export default MainFlow;
