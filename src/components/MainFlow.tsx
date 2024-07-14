89c16078f308ab2a2b118500a21b709698eadeee
KorawutMing <6432004421@student.chula.ac.th>
2024-07-14 11:56:58 +0700

    grap

--- src/components/MainFlow.tsx
+++ src/components/MainFlow.tsx
@@ -1,6 +1,19 @@
 'use client'
 
 import React from "react";
+import transitionPageConfig from "../app/config/transitionPageConfig.json";
+import Image from 'next/image';
+
+type TransitionPageConfig = {
+  [key: string]: {
+    nextPage: string;
+    img1: string;
+    text1: string;
+    img2: string;
+    buttonText: string;
+  };
+};
+const tconfig: TransitionPageConfig = transitionPageConfig;
 
 const MainFlow = () => {
   const [pageIdx, setPageIdx] = React.useState('P1');
@@ -14,8 +27,11 @@ const MainFlow = () => {
 const TransitionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: (idx: string) => void }) => {
   return (
     <div className="bg-green-500">
-      <h1>{pageIdx}</h1>
-      <button onClick={() => setPageIdx('P2')}>Go to next page</button>
+      <h1>{tconfig[pageIdx].text1}</h1>
+      <Image src={tconfig[pageIdx].img1} alt="img1" width={500} height={500}/>
+      <button onClick={() => setPageIdx(tconfig[pageIdx].nextPage)}>
+        tconfig[pageIdx].
+      </button>
     </div>
   );
 };
