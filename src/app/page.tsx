21529ac0f515cd68a7d43c1546f0ee96e63376dd
PannapornT <149418791+PannapornT@users.noreply.github.com>
2024-08-01 23:44:26 +0700

    finish_firstpage_nofont

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,9 +22,9 @@ export default function Home() {
  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p onClick={handleClick} className="cursor-pointer">
+      {/* <p onClick={handleClick} className="cursor-pointer">
         TAP JAAAAAA
-      </p>
+      </p> */}
       {state === 0 && <HomeScreen handleClick={handleClick} />}
       {state === 1 && <MainFlow />}
       {state === 2 && <Result />}
--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -5,20 +5,29 @@ interface HomeScreenProps {
   handleClick: () => void;
 }
 
-const HomeScreen: React.FC<HomeScreenProps> = ({ handleClick }) =>  {
+const HomeScreen: React.FC<HomeScreenProps> = ({ handleClick }) => {
   return (
-    <div className="flex min-h-screen items-center justify-between " onClick={handleClick}>
-        <div >
-          <div className="relative" >
-          <Image src="/images/bg_text_home.png" alt="bg" width={600} height={600} />
+    <div
+      className="flex min-h-screen flex-col items-center justify-center"
+      onClick={handleClick}
+    >
+      <div>
+        <div className="relative">
+          <div className="relative flex-col items-center justify-center">
+            <Image
+              src="/images/bg_text_home.png"
+              alt="bg"
+              width={600}
+              height={600}
+            />
           </div>
-          <div className="absolute inset-0 flex flex-col items-center justify-center text-center" >     
-          Who you are   <br />
-          if you aren't a medical <br />
-          student?
+          <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+            Who you are <br />
+            if you aren&apos;t a medical <br />
+            student?
           </div>
         </div>
-
+      </div>
     </div>
   );
 };
