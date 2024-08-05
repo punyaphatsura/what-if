f74cdafd17179683ed316366d891b16b715437d3
KorawutMing <6432004421@student.chula.ac.th>
2024-08-05 21:04:43 +0700

    feat: :sparkles: basic logic of question page (tentative)

--- src/components/MainFlow.tsx
+++ src/components/MainFlow.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import transitionPageConfig from "../app/config/transitionPageConfig.json";
+import questionPageConfig from "../app/config/questionPageConfig.json";
 import Image from 'next/image';
 
 type TransitionPageConfig = {
@@ -13,8 +14,21 @@ type TransitionPageConfig = {
     buttonText: string;
   };
 };
+type QuestionPageConfig = {
+  [key: string]: {
+    question: string;
+    options: {
+      text: string;
+      nextPage: string;
+      score: string;
+    }[];
+    buttonText: string;
+    img: { path: string, width: number, height: number };
+  };
+};
 
 const tconfig: TransitionPageConfig = transitionPageConfig;
+const qconfig: QuestionPageConfig = questionPageConfig;
 
 const MainFlow = () => {
   const [pageIdx, setPageIdx] = React.useState('P1');
@@ -38,7 +52,7 @@ const TransitionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: 
           className="mb-4"
         />
       )}
-      <h1 className="bg-gray-700 text-white p-4 rounded-lg mb-4">
+      <h1 className="bg-gray-700 text-white p-4 rounded-lg mb-4 text-center">
         {text1}
       </h1>
       {img2.path && (
@@ -61,9 +75,40 @@ const TransitionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: 
 };
 
 const QuestionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: (idx: string) => void }) => {
+  const { question, options, buttonText, img } = qconfig[pageIdx];
   return (
-    <div className="bg-blue-500">QuestionPage</div>
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-300 to-yellow-200 p-10">
+      <h1 className="bg-green-500 text-white text-center p-4 rounded-lg mb-6">
+        {question}
+      </h1>
+      <div className="flex flex-col space-y-4 mb-6">
+        {options.map((option, idx) => (
+          <button
+            key={idx}
+            className="bg-white text-green-500 p-3 rounded-full shadow-md hover:bg-green-100 transition duration-300"
+            onClick={() => setPageIdx(option.nextPage)}
+          >
+            {option.text}
+          </button>
+        ))}
+      </div>
+      <button
+        onClick={() => setPageIdx(options[0].nextPage)}
+        className="bg-green-500 text-white py-2 px-6 rounded-full hover:bg-green-700 transition duration-300"
+      >
+        {buttonText}
+      </button>
+      {img.path && (
+        <Image
+          src={img.path}
+          alt="illustration"
+          width={img.width}
+          height={img.height}
+          className="mt-8"
+        />
+      )}
+    </div>
   );
-}
+};
 
 export default MainFlow;
