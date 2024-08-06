47081836d715066428ea3f9fab42a96995e169d7
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2024-08-06 09:36:21 +0700

    feat: :recycle: Move type to utils

--- src/components/MainFlow.tsx
+++ src/components/MainFlow.tsx
@@ -1,72 +1,60 @@
-'use client'
+"use client";
 
 import React from "react";
 import transitionPageConfig from "../app/config/transitionPageConfig.json";
 import questionPageConfig from "../app/config/questionPageConfig.json";
-import Image from 'next/image';
+import Image from "next/image";
+import {
+  TransitionPageConfigType,
+  QuestionPageConfigType,
+} from "@/utils/types/PageConfig";
 
-type TransitionPageConfig = {
-  [key: string]: {
-    nextPage: string;
-    img1: { path: string, width: number, height: number };
-    text1: string;
-    img2: { path: string, width: number, height: number };
-    buttonText: string;
-  };
-};
-type QuestionPageConfig = {
-  [key: string]: {
-    question: string;
-    options: {
-      text: string;
-      nextPage: string;
-      score: string;
-    }[];
-    buttonText: string;
-    img: { path: string, width: number, height: number };
-  };
-};
-
-const tconfig: TransitionPageConfig = transitionPageConfig;
-const qconfig: QuestionPageConfig = questionPageConfig;
+const tconfig: TransitionPageConfigType = transitionPageConfig;
+const qconfig: QuestionPageConfigType = questionPageConfig;
 
 const MainFlow = () => {
-  const [pageIdx, setPageIdx] = React.useState('P1');
-  if (['P1', 'P2', 'P3', 'P8'].includes(pageIdx)) {
+  const [pageIdx, setPageIdx] = React.useState("P1");
+  if (["P1", "P2", "P3", "P8"].includes(pageIdx)) {
     return <TransitionPage pageIdx={pageIdx} setPageIdx={setPageIdx} />;
   } else {
     return <QuestionPage pageIdx={pageIdx} setPageIdx={setPageIdx} />;
   }
 };
 
-const TransitionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: (idx: string) => void }) => {
+const TransitionPage = ({
+  pageIdx,
+  setPageIdx,
+}: {
+  pageIdx: string;
+  setPageIdx: (idx: string) => void;
+}) => {
   const { nextPage, img1, text1, img2, buttonText } = tconfig[pageIdx];
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-300 to-yellow-200 p-10">
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-purple-300 to-yellow-200 p-10">
       {img1.path && (
-        <Image 
-          src={img1.path} 
-          alt="img1" 
-          width={img1.width} 
-          height={img1.height} 
+        <Image
+          src={img1.path}
+          alt="img1"
+          width={img1.width}
+          height={img1.height}
           className="mb-4"
         />
       )}
-      <h1 className="bg-gray-700 text-white p-4 rounded-lg mb-4 text-center">
+      <h1 className="mb-4 rounded-lg bg-gray-700 p-4 text-center text-white">
         {text1}
       </h1>
       {img2.path && (
-        <Image 
-          src={img2.path} 
-          alt="img2" 
-          width={img2.width} 
-          height={img2.height} 
+        <Image
+          src={img2.path}
+          alt="img2"
+          width={img2.width}
+          height={img2.height}
           className="mb-4"
         />
       )}
-      <button 
+      <button
         onClick={() => setPageIdx(nextPage)}
-        className="bg-green-500 text-white py-2 px-6 rounded-full hover:bg-green-700 transition duration-300"
+        className="rounded-full bg-green-500 px-6 py-2 text-white transition duration-300 hover:bg-green-700"
       >
         {buttonText}
       </button>
@@ -74,18 +62,24 @@ const TransitionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: 
   );
 };
 
-const QuestionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: (idx: string) => void }) => {
+const QuestionPage = ({
+  pageIdx,
+  setPageIdx,
+}: {
+  pageIdx: string;
+  setPageIdx: (idx: string) => void;
+}) => {
   const { question, options, buttonText, img } = qconfig[pageIdx];
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-300 to-yellow-200 p-10">
-      <h1 className="bg-green-500 text-white text-center p-4 rounded-lg mb-6">
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-purple-300 to-yellow-200 p-10">
+      <h1 className="mb-6 rounded-lg bg-green-500 p-4 text-center text-white">
         {question}
       </h1>
-      <div className="flex flex-col space-y-4 mb-6">
+      <div className="mb-6 flex flex-col space-y-4">
         {options.map((option, idx) => (
           <button
             key={idx}
-            className="bg-white text-green-500 p-3 rounded-full shadow-md hover:bg-green-100 transition duration-300"
+            className="rounded-full bg-white p-3 text-green-500 shadow-md transition duration-300 hover:bg-green-100"
             onClick={() => setPageIdx(option.nextPage)}
           >
             {option.text}
@@ -94,7 +88,7 @@ const QuestionPage = ({ pageIdx, setPageIdx }: { pageIdx: string, setPageIdx: (i
       </div>
       <button
         onClick={() => setPageIdx(options[0].nextPage)}
-        className="bg-green-500 text-white py-2 px-6 rounded-full hover:bg-green-700 transition duration-300"
+        className="rounded-full bg-green-500 px-6 py-2 text-white transition duration-300 hover:bg-green-700"
       >
         {buttonText}
       </button>
