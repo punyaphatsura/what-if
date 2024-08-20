b0040afe6fa6bfb879ffb8fe3486d253e9c96448
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2024-08-20 22:00:44 +0700

    Add preload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,33 @@ import MainFlow from '@/components/MainFlow';
 import Result from '@/components/Result';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import transitionPageConfig from '../app/config/transitionPageConfig.json';
+import questionPageConfig from '../app/config/questionPageConfig.json';
+import useImagePreloader from '@/utils/hooks/useImagePreloader'; // Import the preloader hook
+import { TransitionPageConfigType, QuestionPageConfigType } from '@/utils/types/PageConfig';
+
+const tconfig: TransitionPageConfigType = transitionPageConfig;
+const qconfig: QuestionPageConfigType = questionPageConfig;
 
 export default function Home() {
   const [state, setState] = useState(0);
   const [choice, setChoice] = useState('');
   const [transitioning, setTransitioning] = useState(false);
+  const [imageList, setImageList] = useState<string[]>([]);
+
+  useEffect(() => {
+    setImageList(
+      [
+        ...Object.values(tconfig).flatMap((config) => [config.img1.path, config.img2.path]),
+        ...Object.values(qconfig).flatMap((config) => config.img.path),
+        `/result/f-export.png`,
+        `/result/m-export.png`,
+        `/result/t-export.png`,
+      ].filter((path) => path !== '')
+    );
+  }, []);
+
+  const { imagesPreloaded } = useImagePreloader(imageList);
 
   const handleClick = () => {
     setTransitioning(true);
